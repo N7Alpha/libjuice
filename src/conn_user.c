83d720f712ff24fa800b565c909f586d16cbae8f
John Rehbein <john.rehbein@icloud.com>
2023-10-07 21:08:52 -0700

    Remove polling

--- src/conn_user.c
+++ src/conn_user.c
@@ -14,9 +14,6 @@
 
 #include <stdint.h>
 
-#define BUFFER_SIZE 4096
-#define MAX_AGENT (BUFFER_SIZE/sizeof(struct pollfd))
-
 typedef enum conn_state { CONN_STATE_NEW = 0, CONN_STATE_READY, CONN_STATE_FINISHED } conn_state_t;
 
 typedef struct conn_impl {
@@ -29,137 +26,47 @@ typedef struct conn_impl {
 static inline int conn_user_process(juice_agent_t **agents, int agents_count, uint32_t *has_packets_pending, char *buffer);
 static inline int conn_user_recv(socket_t sock, char *buffer, size_t size, addr_record_t *src);
 
-JUICE_EXPORT int juice_user_poll(juice_agent_t **agents, int agents_count) {
-	JLOG_VERBOSE("Setting up poll for %d agents", agents_count);
+JUICE_EXPORT int juice_user_poll(juice_agent_t *agent, char *buffer, size_t buffer_size) {
+	if (!agent || !buffer)
+		return JUICE_ERR_INVALID;
+	
+	conn_impl_t *conn_impl = agent->conn_impl;
 
-	if (agents_count == 0)
-		return JUICE_ERR_SUCCESS;
-
-	if (!agents)
+	if (!conn_impl)
 		return JUICE_ERR_INVALID;
 
-	if (agents_count > MAX_AGENT) {
-		JLOG_ERROR("agent_count > %d", MAX_AGENT);
+	if (conn_impl->state == CONN_STATE_FINISHED)
+		return JUICE_ERR_FAILED;
+
+	if (agent->config.concurrency_mode != JUICE_CONCURRENCY_MODE_USER) {
+		JLOG_ERROR("agent->config.concurrency_mode=%d Only JUICE_CONCURRENCY_MODE_USER (%d) is supported", 
+		            agent->config.concurrency_mode, JUICE_CONCURRENCY_MODE_USER);
 		return JUICE_ERR_INVALID;
 	}
 
-	// For explicit memory reuse
-	union {
-		struct pollfd pfds[MAX_AGENT];
-		char buffer[BUFFER_SIZE];
-	} u;
-	struct pollfd *pfds = u.pfds;
+	addr_record_t src;
+	int ret = conn_user_recv(conn_impl->sock, buffer, buffer_size, &src);
 
-	uint32_t has_packets_pending[(MAX_AGENT-1)/32+1];
-
-	int status = JUICE_ERR_SUCCESS;
-	for(int i = 0; i < agents_count; ++i) {
-		struct pollfd *pfd = pfds + i;
-
-		if (!agents[i]) {
-			JLOG_ERROR("agents[%d] is NULL", i);
-			status = JUICE_ERR_INVALID;
-			continue;
-		}
-
-		if (agents[i]->config.concurrency_mode != JUICE_CONCURRENCY_MODE_USER) {
-			JLOG_ERROR("agents[%d].config.concurrency_mode=%d Only JUICE_CONCURRENCY_MODE_USER (%d) is supported", 
-			            i, agents[i]->config.concurrency_mode, JUICE_CONCURRENCY_MODE_USER);
-			status = JUICE_ERR_INVALID;
-			continue;
-		}
-
-		conn_impl_t *conn_impl = agents[i]->conn_impl;
-		if (conn_impl->state != CONN_STATE_NEW && conn_impl->state != CONN_STATE_READY) {
-			pfd->fd = INVALID_SOCKET;
-			pfd->events = 0;
-			continue;
-		}
-
-		if (conn_impl->state == CONN_STATE_NEW)
-			conn_impl->state = CONN_STATE_READY;
-
-		pfd->fd = conn_impl->sock;
-		pfd->events = POLLIN;
-	}
-
-	if (status != JUICE_ERR_SUCCESS)
-		return status;
-
-	JLOG_VERBOSE("Entering poll on %d sockets", agents_count);
-	while ((poll(pfds, agents_count, 0)) < 0) {
-		// POSIX allows kernels to set these two errors unconditionally when calling poll
-		// In this case looping until poll succeeds is standard practice
-		if (sockerrno == SEINTR || sockerrno == SEAGAIN) {
-			JLOG_VERBOSE("poll interrupted");
-		} else {
-			JLOG_FATAL("poll failed, error=%d", sockerrno);
-		}
-	}
-	JLOG_VERBOSE("Leaving poll");
-
-	for (int i = 0; i < agents_count; ++i) {
-		struct pollfd *pfd = pfds + i;
-
-		if (pfd->revents & POLLNVAL || pfd->revents & POLLERR) {
-			JLOG_WARN("Error when polling socket on agent[%d]", i);
-			agent_conn_fail(agents[i]);
-			conn_impl_t *conn_impl = agents[i]->conn_impl;
+	if (ret < 0) {
+		agent_conn_fail(agent);
+		conn_impl->state = CONN_STATE_FINISHED;
+		return JUICE_ERR_FAILED;
+	} else if (ret > 0) {
+		if (agent_conn_recv(agent, buffer, (size_t)ret, &src) != 0) {
+			JLOG_WARN("Agent receive failed");
 			conn_impl->state = CONN_STATE_FINISHED;
-			continue;
-		}
-
-		uint32_t pending = (pfd->revents & POLLIN) > 0;
-		has_packets_pending[i/32] |= pending << (i%32);
-	}
-
-	// This subroutine protects from accidently accessing pfds
-    return conn_user_process(agents, agents_count, has_packets_pending, u.buffer);
-}
-
-static inline int conn_user_process(juice_agent_t **agents, int agents_count, uint32_t *has_packets_pending, char *buffer) {
-	for (int i = 0; i < agents_count; ++i) {
-		juice_agent_t *agent = agents[i];
-
-		conn_impl_t *conn_impl = agent->conn_impl;
-		if (!conn_impl || conn_impl->state != CONN_STATE_READY)
-			continue;
-
-		if (has_packets_pending[i/32] & (1 << (i%32))) {
-			addr_record_t src;
-			int ret = 0;
-			while ((ret = conn_user_recv(conn_impl->sock, buffer, BUFFER_SIZE, &src)) > 0) {
-				if (agent_conn_recv(agent, buffer, (size_t)ret, &src) != 0) {
-					JLOG_WARN("Agent receive failed");
-					conn_impl->state = CONN_STATE_FINISHED;
-					break;
-				}
-			}
-			if (conn_impl->state == CONN_STATE_FINISHED)
-				continue;
-
-			if (ret < 0) {
-				agent_conn_fail(agent);
-				conn_impl->state = CONN_STATE_FINISHED;
-				continue;
-			}
-
-			if (agent_conn_update(agent, &conn_impl->next_timestamp) != 0) {
-				JLOG_WARN("Agent update failed");
-				conn_impl->state = CONN_STATE_FINISHED;
-				continue;
-			}
-
-		} else if (conn_impl->next_timestamp <= current_timestamp()) {
-			if (agent_conn_update(agent, &conn_impl->next_timestamp) != 0) {
-				JLOG_WARN("Agent update failed");
-				conn_impl->state = CONN_STATE_FINISHED;
-				continue;
-			}
+			return JUICE_ERR_FAILED;
 		}
 	}
 
-	return JUICE_ERR_SUCCESS;
+	// Update the ICE agent... calling this everytime might be expensive
+	if (agent_conn_update(agent, &conn_impl->next_timestamp) != 0) {
+		JLOG_WARN("Agent update failed");
+		conn_impl->state = CONN_STATE_FINISHED;
+		return JUICE_ERR_FAILED;
+	}
+
+	return ret;
 }
 
 static inline int conn_user_recv(socket_t sock, char *buffer, size_t size, addr_record_t *src) {
@@ -254,4 +161,3 @@ int conn_user_get_addrs(juice_agent_t *agent, addr_record_t *records, size_t siz
 
 	return udp_get_addrs(conn_impl->sock, records, size);
 }
-
